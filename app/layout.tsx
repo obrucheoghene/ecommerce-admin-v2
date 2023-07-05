ac9c4d8589ce0f0af299153ed10088345cfaa119
wilfredcloud <wilfredcloudspace@gmail.com>
2023-07-05 09:24:49 +0100

    fix: Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import {ClerkProvider} from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Admin Ecommerce',
+  title: 'Ecommerce Admin',
   description: 'Ecommerce Admin',
 }
 
