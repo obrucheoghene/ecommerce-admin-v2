57844ce0362fb9be7e2229f1ae723dfb4a8361b8
wilfredcloud <wilfredcloudspace@gmail.com>
2023-07-15 09:42:24 +0100

    feat: Consume update store api

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -6,15 +6,16 @@ import * as z from "zod"
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
 
 import { Heading } from "@/components/Heading"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator";
-import { Form, FormControl, 
-    FormField, FormItem,
+import { Form, FormControl, FormField, FormItem,
     FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import axios from "axios";
 
 
 interface SettingsFormProps {
@@ -30,17 +31,26 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
-
-    const [open, setOpen] = useState(false)
-    const [loading, setLoading] = useState(false)
+    const  params = useParams();
+    const router = useRouter();
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
     const form = useForm<SettingsFormValues>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData
     })
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data);
-
+        try {
+            setLoading(true);
+            await axios.patch(`/api/stores/${params.storeId}`, data);
+            router.refresh();
+            toast.success("Store updated")
+        } catch (error) {
+            toast.error("Something went wrong");
+        }finally {
+            setLoading(false);
+        }
     }
 
   return (
@@ -48,9 +58,10 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences"/>
         <Button 
+        disabled={loading}
         variant="destructive"
         size="sm"
-        onClick={() => {}}>
+        onClick={() => setOpen(true)}>
             <Trash className="h-4 w-4"/>
         </Button>
     
